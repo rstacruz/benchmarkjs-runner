496d787c21374078c3dccd288f1c2ee2b2a2a609
Rico Sta. Cruz <hi@ricostacruz.com>
2013-09-09 05:58:39 +0800

    Code.

--- runner.js
+++ runner.js
@@ -12837,22 +12837,35 @@ if ( typeof window === "object" && typeof window.document === "object" ) {
     style: function() {
       return "" +
         "html { background: #fff; color: #111; margin: 40px; padding: 0; }" +
-        "body { font-family: Helvetica Neue, sans-serif; font-size: 12pt; line-height: 1.5; }" +
+        "body { font-size: 12pt; }" +
+        "input, button, body { font-family: Helvetica Neue, sans-serif; line-height: 1.5; }" +
         "button { cursor: pointer; }" +
+        "button { margin: 0; padding: 0; outline: 0; background: transparent; border: 0; }" +
         "h1, h2, h3 { font-weight: 200; font-size: 1em; margin: 0; padding: 0; }" +
         ".b-suite { margin-bottom: 20px; }" +
         ".b-header { margin-bottom: 10px; }" +
         ".b-header h2 { font-size: 1.2em; font-weight: 200; display: inline-block; margin-right: 15px; }" +
-        ".b-header button { margin: 0; padding: 0; outline: 0; background: transparent; border: 0; }" +
-        ".b-header button { padding: 2px 10px; border: solid 1px #eee; border-radius: 2px; }" +
+        ".b-header button { display: inline-block; vertical-align: middle; position: relative; top: -2px; }" +
+        ".b-header button { padding: 1px 10px; border: solid 1px #eee; border-radius: 2px; }" +
+        ".b-header button { border: solid 1px #1bd; border-radius: 15px; color: #1bd; }" +
         ".b-header button:hover { background: #1bd; border-color: #1bd; color: white; }" +
+        ".b-header button:active { background: #222; border-color: #222; color: white; }" +
         ".b-header button.disabled { background: white; border-color: #e0e0e0; color: #bbb; cursor: not-allowed; pointer-events: none; -webkit-pointer-events: none; opacity: 0.2; }" +
         ".b-bench-status { margin-right: 15px; color: #1bd; font-size: 0.7em; }" +
+        ".b-bench { padding: 3px 0; }" +
         ".b-bench h3 { display: inline-block; margin-right: 15px; font-size: 0.9em; }" +
         ".b-progress { display: inline-block; width: 50px; height: 4px; border: solid 1px #ddd; position: relative; border-radius: 2px; margin-right: 10px; padding: 1px; overflow: hidden; }" +
         ".b-progress { -webkit-transform: translate3d(0,0,0); }" +
         ".b-progress-bar { width: 0; background: #ccc; height: 100%; -webkit-transition: width 100ms ease; -moz-transition: width 100ms ease; transition: width 100ms ease; }" +
         ".b-running .b-progress-bar { background: #eee; }" +
+        ".b-expand { background: #f4f4f4; padding: 0 5px; letter-spacing: 1px; line-height: 12px; border: solid 1px transparent; color: #999; border-radius: 2px; }" +
+        ".b-expand:hover { background: #eaeaea; }" +
+        ".b-expand:active { background: #1bd; color: white; } " +
+        ".b-code { padding: 20px; background: #fcfcfc; box-shadow: inset 1px 1px 0 rgba(0, 0, 0, 0.05), inset 0 0 3px rgba(0, 0, 0, 0.05); margin: 10px 0; }" +
+        ".b-code pre { margin: 0; padding: 0; font-family: menlo, ubuntu mono, monospace; font-size: 0.7em; }" +
+        "pre .string, pre .number { color: #1bd; }" +
+        "pre .comment { color: #80808a; }" +
+        "pre .keyword { color: #5a3; }" +
         "";
     },
 
@@ -12873,6 +12886,8 @@ if ( typeof window === "object" && typeof window.document === "object" ) {
           "<div class='b-progress'><div class='b-progress-bar'></div></div>" +
           "<span class='b-bench-status'></span>" +
           "<h3>" + data.name + "</h3>" +
+          "<button class='b-expand'>&middot;&middot;&middot;</button>" +
+          "<div class='b-code' style='display:none'><pre>" + hilite(data.fn.toString().replace(/ +}$/, '}')) + "</pre></div>" +
         "</div>";
     }
   };
@@ -12898,6 +12913,10 @@ if ( typeof window === "object" && typeof window.document === "object" ) {
       $suite.find('.b-run').on('click', function() {
         suite.reset();
         suite.run({ async: true });
+      });
+
+      $suite.find('.b-expand').on('click', function() {
+        $(this).closest('.b-bench').find('.b-code').toggle();
       });
 
       // Bind model -> DOM events
@@ -12982,6 +13001,19 @@ if ( typeof window === "object" && typeof window.document === "object" ) {
     }
     // http://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
     return parseInt(x, 10).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
+  function hilite(str) {
+    return str
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/(".*?")/gm, '<span class="string">$1</span>')
+      .replace(/\/\/(.*)/gm, '<span class="comment">//$1</span>')
+      .replace(/('.*?')/gm, '<span class="string">$1</span>')
+      .replace(/(\d+\.\d+)/gm, '<span class="number">$1</span>')
+      .replace(/(\d+)/gm, '<span class="number">$1</span>')
+      .replace(/\bnew *(\w+)/gm, '<span class="keyword">new</span> <span class="init">$1</span>')
+      .replace(/\b(function|new|throw|return|var|if|else)\b/gm, '<span class="keyword">$1</span>');
   }
 
 })(jQuery);
